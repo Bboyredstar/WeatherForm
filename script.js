8bbe1464a9bd150d66433e2fd9b2d42202c73ba8
Bboyredstar <50084897+Bboyredstar@users.noreply.github.com>
2019-12-04 19:13:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -23,8 +23,8 @@ var mainForm = new Vue({
                 .catch(response => console.log("Error"));
         },
         getElements: function (data) {
-            this.minTemp = data.list[0].temp.min + " °C"
-            this.maxTemp = data.list[0].temp.max + " °C"
+            this.minTemp = data.list[0].temp.min.toFixed(1) + " °C"
+            this.maxTemp = data.list[0].temp.max.toFixed(1) + " °C"
             this.pressure = data.list[0].pressure + ' мм рт. ст.'
             this.windSpeed = data.list[0].speed + ' м./с.'
             this.mainTemp = ((data.list[0].temp.day + data.list[0].temp.morn + data.list[0].temp.eve) / 3).toFixed(1) + " °C"
